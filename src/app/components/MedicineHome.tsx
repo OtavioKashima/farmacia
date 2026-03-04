4bf99b1daddacdb1814dbbb506f1b06e2ee47dd7
Otavio Kashima <43879875880@escola928.edu>
2026-03-04 10:34:07 -0300

    Dependa bot

--- src/app/components/MedicineHome.tsx
+++ src/app/components/MedicineHome.tsx
@@ -1,13 +1,13 @@
 interface Props {
-    name: string;
-    quantity: number;
-  }
-  
-  export default function MedicineItem({ name, quantity }: Props) {
-    return (
-      <div className="medicine-item">
-        <span>{name}</span>
-        <span className="medicine-qty">{quantity}</span>
-      </div>
-    );
-  }
+  name: string;
+  quantity: number;
+}
+
+export default function MedicineItem({ name, quantity }: Props) {
+  return (
+    <div className="medicine-item">
+      <span>{name}</span>
+      <span className="medicine-qty">{quantity}</span>
+    </div>
+  );
+}
